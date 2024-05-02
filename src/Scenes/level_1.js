a19e6233223b003231266bb936b3e2164cb10932
CynthiaHe0 <67443885+CynthiaHe0@users.noreply.github.com>
2024-05-01 22:31:05 -0700

    Added badly drawn Ice_crystal enemy Added player to the screen and let them move using either A/date or left/right arrows

--- src/Scenes/level_1.js
+++ src/Scenes/level_1.js
@@ -1,10 +1,31 @@
-class level_1 extends Phaser.Scenes {
+class Level_1 extends Phaser.Scene {
     constructor(){
         super("level_1");
+        this.my = {sprite: {}};
     }
     preload(){
         //Preload the images
+        this.load.setPath("./assets");
+        this.load.image("cloud_enemy", "cloud.png");
+        this.load.image("lighting", "lighting_blue.png");
+        this.load.image("player", "sun1.png");
+        this.load.image("crystal_enemy", "Ice_crystal.png");
+        this.load.image("fireball", "flame.png");
     }
-    create(){}
-    update(){}
+    create(){
+        let my = this.my;
+
+        this.left1 = this.input.keyboard.addKey("A");
+        this.right1 = this.input.keyboard.addKey("D");
+        this.left2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        this.right2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+        my.sprite.player = new Player(this, game.config.width/2, game.config.height - 40, "player", null,
+        [this.left1, this.left2], [this.right1, this.right2], 5);
+        my.sprite.player.setScale(0.5);
+    }
+    update(){
+        let my = this.my;
+        my.sprite.player.update();
+    }
 }
--- src/Sprites/Player.js
+++ src/Sprites/Player.js
@@ -1,11 +1,14 @@
 //This player sprite implementation was copied from Professor Whitehead's player sprite
 class Player extends Phaser.GameObjects.Sprite{
-    //leftKey and rightKey need to be listeners for the key press
+    //I changed leftKey and rightKey so they need to be an array of listeners for the keys
     constructor(scene, x, y, texture, frame, leftKey, rightKey, playerSpeed){
+        //x and y are the boundaries of the game screen
         super(scene, x, y, texture, frame);
         //Still not sure what super does but I assume it has to do with inheritance?
-        this.left = leftKey;
-        this.right = rightKey;
+        this.left1 = leftKey[0];
+        this.left2 = leftKey[1];
+        this.right1 = rightKey[0];
+        this.right2 = rightKey[1];
         this.playerSpeed = playerSpeed;
 
         scene.add.existing(this);
@@ -14,7 +17,7 @@ class Player extends Phaser.GameObjects.Sprite{
     }
     update(){
         // Moving left
-        if (this.left.isDown) {
+        if (this.left1.isDown || this.left2.isDown) {
             // Check to make sure the sprite can actually move left
             if (this.x > (this.displayWidth/2)) {
                 this.x -= this.playerSpeed;
@@ -22,7 +25,7 @@ class Player extends Phaser.GameObjects.Sprite{
         }
 
         // Moving right
-        if (this.right.isDown) {
+        if (this.right1.isDown || this.right2.isDown) {
             // Check to make sure the sprite can actually move right
             if (this.x < (game.config.width - (this.displayWidth/2))) {
                 this.x += this.playerSpeed;
